7f55a805f58f572b72c43a73c507f18d6e1900c0
John Thompson <john@springframework.guru>
2017-10-19 13:27:43 -0400

    adding webFlux client

---
+++ src/main/java/guru/springframework/mongodbreactivestockquoteservice/client/StockQuoteClient.java
@@ -0,0 +1,40 @@
+package guru.springframework.mongodbreactivestockquoteservice.client;
+
+import guru.springframework.mongodbreactivestockquoteservice.domain.Quote;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+
+/**
+ * Created by jt on 10/19/17.
+ */
+@Slf4j
+@Setter
+@Component
+@ConfigurationProperties("guru")
+public class StockQuoteClient {
+
+    private String host;
+    private String port;
+    private String path;
+
+    public Flux<Quote> getQuoteStream(){
+
+        String url = "http://" + host + port;
+
+        log.debug("Url Set is: " + url);
+
+        return WebClient.builder()
+                .baseUrl(url)
+                .build()
+                .get()
+                .uri(path)
+                .accept(MediaType.APPLICATION_STREAM_JSON)
+                .retrieve()
+                .bodyToFlux(Quote.class);
+    }
+}
