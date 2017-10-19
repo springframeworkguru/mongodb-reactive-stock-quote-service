a09ff04e3d8c06aeca2ada797faa342b3e2c04c3
John Thompson <john@springframework.guru>
2017-10-19 15:13:05 -0400

    adding tailable cursor example

--- src/main/java/guru/springframework/mongodbreactivestockquoteservice/QuoteRunner.java
+++ src/main/java/guru/springframework/mongodbreactivestockquoteservice/QuoteRunner.java
@@ -2,25 +2,35 @@ package guru.springframework.mongodbreactivestockquoteservice;
 
 import guru.springframework.mongodbreactivestockquoteservice.client.StockQuoteClient;
 import guru.springframework.mongodbreactivestockquoteservice.domain.Quote;
+import guru.springframework.mongodbreactivestockquoteservice.repositories.QuoteRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 
 /**
  * Created by jt on 10/19/17.
  */
-//@Component
+@Component
 public class QuoteRunner implements CommandLineRunner {
 
     private final StockQuoteClient stockQuoteClient;
+    private final QuoteRepository repository;
 
-    public QuoteRunner(StockQuoteClient stockQuoteClient) {
+    public QuoteRunner(StockQuoteClient stockQuoteClient, QuoteRepository repository) {
         this.stockQuoteClient = stockQuoteClient;
+        this.repository = repository;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        Flux<Quote> quoteFlux = stockQuoteClient.getQuoteStream();
 
-        quoteFlux.subscribe(System.out::println);
+        Flux<Quote> quoteFlux = repository.findWithTailableCursorBy();
+
+        Disposable disposable = quoteFlux.subscribe(quote -> {
+            System.out.println("*#*#*#*#*#*#*#*#*#*#*#*#* Id: " + quote.getId());
+        });
+
+        disposable.dispose();
     }
 }
--- src/main/java/guru/springframework/mongodbreactivestockquoteservice/repositories/QuoteRepository.java
+++ src/main/java/guru/springframework/mongodbreactivestockquoteservice/repositories/QuoteRepository.java
@@ -2,10 +2,15 @@ package guru.springframework.mongodbreactivestockquoteservice.repositories;
 
 import guru.springframework.mongodbreactivestockquoteservice.domain.Quote;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.Tailable;
+import reactor.core.publisher.Flux;
 
 
 /**
  * Created by jt on 10/19/17.
  */
 public interface QuoteRepository extends ReactiveMongoRepository<Quote, String> {
+
+    @Tailable
+    Flux<Quote> findWithTailableCursorBy(); //must be capped collection
 }
